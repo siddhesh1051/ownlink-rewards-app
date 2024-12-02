38b45e33f767c1272ab533b84d0efde11ade479f
siddhesh-turbostart <siddhesh.p@tsquaredc.com>
2024-12-03 03:16:27 +0530

    changed backend url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
-const BACKEND_URL = "https://443a-103-215-237-88.ngrok-free.app/api";
-// const BACKEND_URL = "https://ownlink-server.onrender.com/api";
+// const BACKEND_URL = "https://443a-103-215-237-88.ngrok-free.app/api";
+const BACKEND_URL = "https://ownlink-server.onrender.com/api";
 export { BACKEND_URL };
